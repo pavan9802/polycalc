c044b56164233dd613f8b89d0576b8eaf8f2a542
pavan9802 <68879257+pavan9802@users.noreply.github.com>
2021-10-25 16:18:52 -0400

    Create term.java

---
+++ term.java
@@ -0,0 +1,45 @@
+package poly;
+
+/**
+ * implements a polynomial.
+
+ */
+public class Term {
+	
+	/**
+	 * Coefficient of term.
+	 */
+	public float coeff;
+	
+	/**
+	 * Degree of term.
+	 */
+	public int degree;
+	
+	/**
+	 * Initializes an instance of term class
+	 */
+	public Term(float coeff, int degree) {
+		this.coeff = coeff;
+		this.degree = degree;
+	}
+	
+	
+	public boolean equals(Object other) {
+		return other != null &&
+		other instanceof Term &&
+		coeff == ((Term)other).coeff &&
+		degree == ((Term)other).degree;
+	}
+	
+	
+	public String toString() {
+		if (degree == 0) {
+			return coeff + "";
+		} else if (degree == 1) {
+			return coeff + "x";
+		} else {
+			return coeff + "x^" + degree;
+		}
+	}
+}
